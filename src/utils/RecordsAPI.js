4feff4ea1c25e36e80ac207efe757b8d37b2dcda
huayezhiwu <huayezhiwu@sina.cn>
2018-04-25 20:21:12 +0800

    2

--- src/utils/RecordsAPI.js
+++ src/utils/RecordsAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const api = process.env.REACT_APP_RECORDS_API_URL || 'http://localhost:5000';
+// export const api = process.env.REACT_APP_RECORDS_API_URL || 'http://localhost:5000';
+export const api = "https://5ad99ad054db9a0014e49279.mockapi.io";
 
 export const getAll = () => 
     axios.get(`${api}/api/v1/records`)
